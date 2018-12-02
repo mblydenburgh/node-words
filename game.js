07b0c3f4a55c000f2f57e53e537662baed86747d
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-12-02 11:28:17 -0500

    user prompt and guessed letters array

--- game.js
+++ game.js
@@ -1,8 +1,11 @@
+const inquirer = require('inquirer');
 const Letter = require('./Letter');
 const Word = require ('./Word');
 const words = ["test","snake","two words"];
 
 let randomIndex;
+let guessedLetters = [];
+let selectedWord;
 
 function chooseWord(arr){
     randomIndex = randomNumber(arr.length);
@@ -13,7 +16,44 @@ function randomNumber(num){
     return Math.floor(Math.random()*num);
 }
 
-let word1 = new Word(chooseWord(words));
+function startGame(){
+    console.log(`Welcome to word guess!`);
+    console.log(`Selecting word....`);
+    selectedWord = new Word(chooseWord(words));
+    setTimeout(()=>{promptGuess()},2000)
+}
+
+function promptGuess(){
+    inquirer.prompt([
+        {
+            name:"userGuess",
+            message:"Please guess a letter or type 'quit' to exit:"
+        }
+    ])
+    .then(function(response){
+        const {userGuess:guess} = response;
+        switch (guess){
+            case 'quit':
+                console.log(`Thanks for coming!`);
+                break;
+            default:
+                if(guess.length>1){
+                    console.log(`Please enter 1 letter`);
+                    promptGuess();
+                }
+                else{
+                    console.log(`guess:${guess}`);
+                    guessedLetters.push(guess);
+                    console.log(`guessed: ${guessedLetters}`)
+                    promptGuess();
+                }
+        }
+    })
+}
+
+startGame();
+
+// let word1 = new Word(chooseWord(words));
 // word1.returnWord();
 // word1.checkGuess('e');
 // word1.returnWord();
